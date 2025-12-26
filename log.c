c50f5f389d1eeb2c74487720467aa058438d68d9
Adam Foxman <5109471+afoxman@users.noreply.github.com>
2025-12-25 16:52:04 -0800

    Finish log implementation and add tests.

--- log.c
+++ log.c
@@ -3,6 +3,8 @@
  */
 
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
 #include <syslog.h>
 
 #include "log.h"
@@ -63,7 +65,7 @@ static int get_log_level_as_syslog_level(log_level_t level) {
   return LOG_INFO;
 }
 
-static log_t* log_alloc() {
+static log_t* log_alloc(void) {
   log_t *log = malloc(sizeof(log_t));
   if (log)
     memset(log, 0, sizeof(*log));
@@ -73,7 +75,7 @@ static log_t* log_alloc() {
 static void log_free(log_t **plog) {
   log_t *log = *plog;
   if (log) {
-    free(log->prefix);
+    free((char*)log->prefix);
     free(log);
     *plog = NULL;
   }
@@ -102,7 +104,7 @@ log_t *log_create_using_syslog(log_level_t minimum_level, const char *prefix, in
 
   log_t *log = log_alloc();
   if (log) {
-    log->output_type - log_output_type_syslog;
+    log->output_type = log_output_type_syslog;
     log->syslog_facility = facility & LOG_FACMASK;
     log->minimum_level = minimum_level;
     log->prefix = prefix ? strdup(prefix) : NULL;
@@ -115,30 +117,33 @@ void log_destroy(log_t **plog) {
   log_free(plog);
 }
 
+ATTRIBUTE_FORMAT(printf, 3, 0)
 static size_t format_string_v(char *buffer, size_t length, const char *format, va_list ap) {
   int count = vsnprintf(buffer, length, format, ap);
   if (count < 0)
     count = 0;
-  else if (count > length)
-    count = length;
+  else if ((size_t)count > length)
+    count = (int)length;
 
-  if (count < length)
+  if ((size_t)count < length)
     buffer[count] = '\0';
 
   return count;
 }
 
+ATTRIBUTE_FORMAT(printf, 3, 4)
 static size_t format_string(char *buffer, size_t length, const char *format, ...) {
   va_list ap;
-  int count;
+  size_t count;
 
   va_start(ap, format);
-  int count = format_string_v(buffer, length, format, ap);
+  count = format_string_v(buffer, length, format, ap);
   va_end(ap);
 
   return count;
 }
 
+ATTRIBUTE_FORMAT(printf, 8, 0)
 static void format_log_message(
     char *buffer, size_t buffer_len,
     const char *prefix, 
@@ -166,6 +171,7 @@ static void format_log_message(
   format_string_v(buffer, buffer_len, format, ap);
 }
 
+ATTRIBUTE_FORMAT(printf, 6, 0)
 static void log_message_internal(
     const log_t *log, log_level_t level, 
     const char *filename, int line, const char *function,
@@ -182,7 +188,7 @@ static void log_message_internal(
   }
 }
 
-const char *get_basename(const char *filename) {
+static const char *get_basename(const char *filename) {
   const char *last_slash;
 
   if (!filename)
--- tests/test_log.c
+++ tests/test_log.c
@@ -3,12 +3,189 @@
  */
 
 #undef NDEBUG
+#define DEBUG_PAM // Enable file/line/function data in log messages
 
-#include <stdio.h>
 #include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 #include "log.h"
 
+#define assert_var_ex(type, name, expr, bad)  type name = expr; assert(name != bad)
+#define assert_var(type, var, expr)           assert_var_ex(type, var, expr, NULL)
+#define assert_ok(expr)                       assert(0 == (expr))
+
+/* Stub to replace syslog() for testing */
+
+FILE *syslog_file = NULL;
+
+ATTRIBUTE_FORMAT(printf, 2, 3)
+void syslog(int priority, const char *format, ...) {
+  va_list ap;
+  fprintf(syslog_file, "priority=%d ", priority);
+  va_start(ap, format);
+  vfprintf(syslog_file, format, ap);
+  fprintf(syslog_file, "\n");
+  va_end(ap);
+}
+
+static FILE *open_tmp_file(const char *name) {
+  char template[256];
+  snprintf(template, sizeof(template), "%sXXXXXX", name);
+
+  assert_var_ex(int, fd, mkstemp(template), -1);
+  assert_var(FILE*, file, fdopen(fd, "w+"));
+
+  // Try to remove file now. It will stay open, but will not be accessible through the filesystem.
+  // Space will be freed when the file is closed.
+  unlink(template);
+
+  return file;
+}
+
+static void rewind_file(FILE *file) {
+  assert_ok(fflush(file));
+  assert_ok(fseek(file, 0, SEEK_SET));
+}
+
+static bool next_line_contains(FILE *file, const char *s) {
+  char buffer[2500];
+  if (!fgets(buffer, sizeof(buffer), file))
+    buffer[0] = '\0';
+  return NULL != strstr(buffer, s);
+}
+
+static bool file_is_empty(FILE *file) {
+  struct stat st;
+  assert_ok(fstat(fileno(file), &st));
+  return st.st_size == 0;
+}
+
+static void test_file_trace_is_logged(void) {
+  const char *message = "This is a test trace message";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  assert_var(log_t*, log, log_create_using_file(log_level_trace, __func__, file));
+  log_trace(log, "%s", message);
+  log_destroy(&log);
+  rewind_file(file);
+  assert(next_line_contains(file, message));
+  assert_ok(fclose(file));
+}
+
+static void test_file_warn_is_logged(void) {
+  const char *message = "This is a test warning message";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  assert_var(log_t*, log, log_create_using_file(log_level_trace, __func__, file));
+  log_warn(log, "%s", message);
+  log_destroy(&log);
+  rewind_file(file);
+  assert(next_line_contains(file, message));
+  assert_ok(fclose(file));
+}
+
+static void test_file_info_is_skipped(void) {
+  const char *message = "This info message should not be logged";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  assert_var(log_t*, log, log_create_using_file(log_level_warn, __func__, file));
+  log_info(log, "%s", message);
+  log_destroy(&log);
+  assert(file_is_empty(file));
+  assert_ok(fclose(file));
+}
+
+static void test_syslog_info_is_logged(void) {
+  const char *message = "This info message should be sent to syslog";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  syslog_file = file;
+  assert_var(log_t*, log, log_create_using_syslog(log_level_trace, __func__, LOG_USER));
+  log_info(log, "%s", message);
+  log_destroy(&log);
+  rewind_file(file);
+  assert(next_line_contains(file, message));
+  assert_ok(fclose(file));
+  syslog_file = NULL;
+}
+
+static void test_syslog_trace_is_skipped(void) {
+  const char *message = "This trace message should NOT be sent to syslog";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  syslog_file = file;
+  assert_var(log_t*, log, log_create_using_syslog(log_level_info, __func__, LOG_USER));
+  log_trace(log, "%s", message);
+  log_destroy(&log);
+  rewind_file(file);
+  assert(file_is_empty(file));
+  assert_ok(fclose(file));
+  syslog_file = NULL;
+}
+
+static void test_file_message_contains_prefix(void) {
+  const char *prefix = "test-log-prefix";
+  const char *message = "This is a test message";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  assert_var(log_t*, log, log_create_using_file(log_level_trace, prefix, file));
+  log_info(log, "%s", message);
+  log_destroy(&log);
+  rewind_file(file);
+  assert(next_line_contains(file, prefix));
+  assert_ok(fclose(file));
+}
+
+#ifdef DEBUG_PAM
+
+static void test_file_message_contains_source_file(void) {
+  const char *message = "This is a test message";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  assert_var(log_t*, log, log_create_using_file(log_level_trace, __func__, file));
+  log_info(log, "%s", message);
+  log_destroy(&log);
+  rewind_file(file);
+  assert(next_line_contains(file, __FILE__));
+  assert_ok(fclose(file));
+}
+
+static void test_file_message_contains_source_function(void) {
+  const char *message = "This is a test message";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  assert_var(log_t*, log, log_create_using_file(log_level_trace, __func__, file));
+  log_info(log, "%s", message);
+  log_destroy(&log);
+  rewind_file(file);
+  assert(next_line_contains(file, __func__));
+  assert_ok(fclose(file));
+}
+
+#endif // DEBUG_PAM
+
+static void test_file_message_contains_log_level_name(void) {
+  const char *message = "This is a test message";
+  assert_var(FILE*, file, open_tmp_file(__func__));
+  assert_var(log_t*, log, log_create_using_file(log_level_trace, __func__, file));
+  log_info(log, "%s", message);
+  log_destroy(&log);
+  rewind_file(file);
+  assert(next_line_contains(file, "INFO:"));
+  assert_ok(fclose(file));
+}
+
 int main(void) {
+  test_file_trace_is_logged();
+  test_file_warn_is_logged();
+  test_file_info_is_skipped();
+
+  test_syslog_info_is_logged();
+  test_syslog_trace_is_skipped();
+
+#ifdef DEBUG_PAM
+  test_file_message_contains_prefix();
+  test_file_message_contains_source_file();
+  test_file_message_contains_source_function();
+#endif // DEBUG_PAM
+  test_file_message_contains_log_level_name();
+
   return 0;
 }
